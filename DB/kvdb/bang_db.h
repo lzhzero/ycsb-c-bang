c2b512d5e50d562a970b0aab41301ef0d2ce8c26
Zhang Liu <zhang.liu@colorado.edu>
2017-10-30 06:42:13 -0600

    bangdb handle

--- DB/kvdb/bang_db.h
+++ DB/kvdb/bang_db.h
@@ -1,25 +1,27 @@
 /*
  * Author: Ning Gao(nigo9731@colorado.edu)
+ *         Zhang Liu(zhli5318@colorado.edu)
  *
- * Hyperdex DB client Handler
+ * BangDB client Handler
  *  connect to the coordinator not the other slave nodes
  *
- * default table has one attribute called "value"
- * space:
+ * NOTE: server IP and port will be loaded from bangdb.config during runtime
+ *
+ * NOTE:client_ is databace class in bangdb_client Namespace
+ *
  * key: {"value": "valuestr"}
  */
 
 #ifndef DTRANX_BANG_DB_H_
 #define DTRANX_BANG_DB_H_
 
-//#include <hyperdex/client.h>
 #include <bangdb-client/database.h>
+#include "kvdb.h"
+
 #include "DB/commons.h"
 namespace Ycsb {
 namespace DB {
 
-static std::string SPACE = "ning";
-static std::string ATTR = "value";
 
 class BangDB: public KVDB {
 public:
@@ -27,14 +29,15 @@ public:
             : client_() {
         shareDB = false;
         keyType = KeyType::STRING;
+        tbl_ = NULL;
     }
 
     BangDB(const BangDB& other) {
         std::cout << "BangDB copy contructor is called" << std::endl;
-        ips_ = other.ips_;
         shareDB = other.shareDB;
         keyType = other.keyType;
         client_ = NULL;
+        tbl_ = NULL;
     }
 
     ~BangDB() {
@@ -44,77 +47,74 @@ public:
     KVDB* Clone(int index) {
         BangDB *instance = new BangDB(*this);
         std::cout << "Cloning BangDB called" << std::endl;
-        instance->client_ = hyperdex_client_create(ips_[0].c_str(), HYPERDEX_SERVER_PORT);
+        instance->client_ = bangdb_client::database((char*)"mydb", NULL, bangdb_client::DB_OPTIMISTIC_TRANSACTION, NULL);
+        bangdb_client::table_env tenv;
+
+        //configure table properties
+        //tenv.set_key_size_byte();
+        tenv.set_persist_type(bangdb_client::PERSIST_ONLY);
+        tenv.set_autocommit_state(1);
+
+        if((instance->tbl_ = client_->gettable((char*)"mytbl", bangdb_client::OPENCREATE, &tenv)) == NULL)
+        {
+            printf("table NULL error, quitting");
+            exit(-1);
+        }
         return instance;
     }
 
     void Init(std::vector<std::string> ips, std::string selfAddress, int localStartPort,
             bool fristTime) {
-        ips_ = ips;
-        assert(ips_.size() > 0);
-        client_ = hyperdex_client_create(ips[0].c_str(), HYPERDEX_SERVER_PORT);
+        client_ = bangdb_client::database((char*)"mydb", NULL, bangdb_client::DB_OPTIMISTIC_TRANSACTION, NULL);
+        bangdb_client::table_env tenv;
+
+        //configure table properties
+        //tenv.set_key_size_byte();
+        tenv.set_persist_type(bangdb_client::PERSIST_ONLY);
+        tenv.set_autocommit_state(true);
+
+        if((tbl_ = client_->gettable((char*)"mytbl", bangdb_client::OPENCREATE, &tenv)) == NULL)
+        	{
+        		printf("table NULL error, quitting");
+        		exit(-1);
+        	}
     }
 
     void Close() {
         if (client_) {
-            hyperdex_client_destroy(client_);
+            client_->closedatabase();
         }
     }
 
     int Read(std::vector<std::string> keys) {
-        enum hyperdex_client_returncode tranx_status;
-        enum hyperdex_client_returncode loop_status;
-        int64_t code = -1;
-        int64_t loopCode = -1;
-        int triedTimes = 0;
-        struct hyperdex_client_transaction* tranx = hyperdex_client_begin_transaction(client_);
-        struct hyperdex_client_attribute* attrs;
-        size_t attrs_sz = 0;
+    		bangdb_client::connection *conn = tbl_->getconnection();
+    		if(!conn)
+    		{
+    			printf("connection null error, please change the max conn config in bangdb.config, it could be the reason\n");
+    			return -1;
+    		}
+
+    		bangdb_client::bangdb_txn* txn;
+    		client_->begin_transaction(txn);
+
         for (auto it = keys.begin(); it != keys.end(); ++it) {
-            tranx_status = HYPERDEX_CLIENT_NOTFOUND;
-            attrs_sz = 0;
-            code = hyperdex_client_xact_get(tranx, SPACE.c_str(), it->c_str(), it->size(),
-                    &tranx_status, (const hyperdex_client_attribute**) &attrs, &attrs_sz);
-            triedTimes = 0;
-            while (loopCode != code) {
-                loopCode = hyperdex_client_loop(client_, 0, &loop_status);
-                triedTimes++;
-                if (triedTimes == 60) {
-                    break;
-                }
-                usleep(50);
-            }
-            if (tranx_status != HYPERDEX_CLIENT_SUCCESS) {
-                /*
-                 std::cout << "reading " << (*it) << " failure "
-                 << hyperdex_client_returncode_to_string(tranx_status)
-                 << " "
-                 << hyperdex_client_returncode_to_string(loop_status)
-                 << std::endl;
-                 */
-                hyperdex_client_abort_transaction(tranx);
-                return kErrorNoData;
-            }
-            if (attrs_sz != 0) {
-                hyperdex_client_destroy_attrs(attrs, attrs_sz);
-            }
+
+        		bangdb_client::FDT ikey, *ival = NULL;
+        		ikey.data = it->c_str();
+        		ikey.length = it->size();
+
+        		if((ival = conn->get(&ikey, txn)) == NULL) {
+        	    		client_->abort_transaction(txn);
+        			printf("individual get transaction failed ");
+        	    		return kErrorNoData;
+        	    	}
         }
-        tranx_status = HYPERDEX_CLIENT_ABORTED;
-        code = hyperdex_client_commit_transaction(tranx, &tranx_status);
-        triedTimes = 0;
-        while (loopCode != code) {
-            loopCode = hyperdex_client_loop(client_, 0, &loop_status);
-            triedTimes++;
-            if (triedTimes == 60) {
-                break;
-            }
-            usleep(50);
-        }
-        if (tranx_status == HYPERDEX_CLIENT_COMMITTED || tranx_status == HYPERDEX_CLIENT_SUCCESS) {
-            return kOK;
-        } else {
-            return kErrorConflict;
-        }
+        if (client_->commit_transaction(txn) >= 0) {
+        		return kOK;
+        	} else {
+        		return kErrorConflict;
+        	}
+
     }
 
     int ReadSnapshot(std::vector<std::string> keys) {
@@ -125,172 +125,112 @@ public:
     }
 
     int Update(std::vector<KVPair> writes) {
-        enum hyperdex_client_returncode tranx_status;
-        enum hyperdex_client_returncode loop_status;
-        int64_t code = -1;
-        int64_t loopCode = -1;
-        int triedTimes = 0;
-        struct hyperdex_client_transaction* tranx = hyperdex_client_begin_transaction(client_);
 
-        enum hyperdex_client_returncode op_status;
-        struct hyperdex_client_attribute attr;
+		bangdb_client::connection *conn = tbl_->getconnection();
+		if(!conn)
+		{
+			printf("connection null error, please change the max conn config in bangdb.config, it could be the reason\n");
+			return -1;
+		}
 
-        for (auto it = writes.begin(); it != writes.end(); ++it) {
-            code = hyperdex_client_xact_put(tranx, SPACE.c_str(), it->first.c_str(),
-                    it->first.size(), &attr, 1, &op_status);
-            triedTimes = 0;
-            while (loopCode != code) {
-                loopCode = hyperdex_client_loop(client_, 0, &loop_status);
-                triedTimes++;
-                if (triedTimes == 60) {
-                    break;
-                }
-                usleep(50);
-            }
-        }
-        code = hyperdex_client_commit_transaction(tranx, &tranx_status);
-        triedTimes = 0;
-        while (loopCode != code) {
-            loopCode = hyperdex_client_loop(client_, 0, &loop_status);
-            triedTimes++;
-            if (triedTimes == 60) {
-                break;
-            }
-            usleep(50);
-        }
+		bangdb_client::bangdb_txn* txn;
+		client_->begin_transaction(txn);
 
-        if (tranx_status == HYPERDEX_CLIENT_COMMITTED || tranx_status == HYPERDEX_CLIENT_SUCCESS) {
-            return kOK;
-        } else {
-            return kErrorConflict;
-        }
+		for (auto it = writes.begin(); it != writes.end(); ++it) {
+    			bangdb_client::FDT ikey, ival;
+    			ikey.data = it->first.c_str();
+    			ikey.length = it->first.size();
+    			ival.data = it->second.c_str();
+    			ival.length = it->second.size();
+    			if(conn->put(&ikey, &ival, bangdb_client::INSERT_UNIQUE, txn) < 0) {
+    				printf("individual update transaction failed ");
+    				exit(1);
+
+    			}
+		}
+
+		if (client_->commit_transaction(txn) >= 0) {
+			return kOK;
+		} else {
+			return kErrorConflict;
+		}
     }
 
     int ReadWrite(std::vector<std::string> reads, std::vector<KVPair> writes) {
-        enum hyperdex_client_returncode tranx_status;
-        enum hyperdex_client_returncode loop_status;
-        enum hyperdex_client_returncode op_status;
-        int64_t code = -1;
-        int64_t loopCode = -1;
-        int triedTimes = 0;
-        struct hyperdex_client_transaction* tranx = hyperdex_client_begin_transaction(client_);
 
-        struct hyperdex_client_attribute attr;
-        struct hyperdex_client_attribute* attrs;
-        size_t attrs_sz = 0;
+    		bangdb_client::connection *conn = tbl_->getconnection();
+    		if(!conn) {
+    			printf("connection null error, please change the max conn config in bangdb.config, it could be the reason\n");
+    			return -1;
+    		}
 
-        for (auto it = reads.begin(); it != reads.end(); ++it) {
-            tranx_status = HYPERDEX_CLIENT_NOTFOUND;
-            attrs_sz = 0;
-            code = hyperdex_client_xact_get(tranx, SPACE.c_str(), it->c_str(), it->size(),
-                    &tranx_status, (const hyperdex_client_attribute**) &attrs, &attrs_sz);
-            triedTimes = 0;
-            while (loopCode != code) {
-                loopCode = hyperdex_client_loop(client_, 0, &loop_status);
-                triedTimes++;
-                if (triedTimes == 60) {
-                    break;
-                }
-                usleep(50);
-            }
-            if (tranx_status != HYPERDEX_CLIENT_SUCCESS) {
-                //std::cout << "reading " << (*it) << " failure" << std::endl;
-                hyperdex_client_abort_transaction(tranx);
-                return kErrorNoData;
-            }
-            if (attrs_sz != 0) {
-                hyperdex_client_destroy_attrs(attrs, attrs_sz);
-            }
+    		bangdb_client::bangdb_txn* txn;
+    		client_->begin_transaction(txn);
+
+    		for (auto it = reads.begin(); it != reads.end(); ++it) {
+    			bangdb_client::FDT ikey, *ival = NULL;
+    			ikey.data = it->c_str();
+    			ikey.length = it->size();
+
+       		if((ival = conn->get(&ikey, txn)) == NULL) {
+        	    		printf("individual read transaction failed ");
+        	    		return kErrorNoData;
+        	    	}
         }
         for (auto it = writes.begin(); it != writes.end(); ++it) {
-            attr.attr = ATTR.c_str();
-            attr.value = it->second.c_str();
-            attr.datatype = hyperdatatype::HYPERDATATYPE_STRING;
-            attr.value_sz = it->second.size();
-            op_status = HYPERDEX_CLIENT_NOTFOUND;
-            code = hyperdex_client_xact_put(tranx, SPACE.c_str(), it->first.c_str(),
-                    it->first.size(), &attr, 1, &op_status);
-            triedTimes = 0;
-            while (loopCode != code) {
-                loopCode = hyperdex_client_loop(client_, 0, &loop_status);
-                triedTimes++;
-                if (triedTimes == 60) {
-                    break;
-                }
-                usleep(50);
-            }
-            if (op_status != HYPERDEX_CLIENT_SUCCESS) {
-                /*
-                 std::cout << "writing " << (it->first) << " failure"
-                 << std::endl;
-                 */
-                hyperdex_client_abort_transaction(tranx);
-                return kErrorNoData;
-            }
-        }
-        tranx_status = HYPERDEX_CLIENT_ABORTED;
-        code = hyperdex_client_commit_transaction(tranx, &tranx_status);
-        triedTimes = 0;
-        while (loopCode != code) {
-            loopCode = hyperdex_client_loop(client_, 0, &loop_status);
-            triedTimes++;
-            if (triedTimes == 60) {
-                break;
-            }
-            usleep(50);
-        }
+        		bangdb_client::FDT ikey, ival;
+        	    ikey.data = it->first.c_str();
+        	    ikey.length = it->first.size();
+        	    ival.data = it->second.c_str();
+        	    ival.length = it->second.size();
+        	    if(conn->put(&ikey, &ival, bangdb_client::INSERT_UNIQUE, txn) < 0) {
+        	        printf("individual write transaction failed ");
+        	        	exit(1);
 
-        if (tranx_status == HYPERDEX_CLIENT_COMMITTED || tranx_status == HYPERDEX_CLIENT_SUCCESS) {
-            return kOK;
-        } else {
-            return kErrorConflict;
+        	    }
         }
+        if (client_->commit_transaction(txn) >= 0) {
+        		return kOK;
+        	} else {
+        		return kErrorConflict;
+        	}
     }
 
     int Insert(std::vector<KVPair> writes) {
-        enum hyperdex_client_returncode tranx_status;
-        enum hyperdex_client_returncode loop_status;
-        int64_t code = -1;
-        int64_t loopCode = -1;
-        int triedTimes = 0;
-        struct hyperdex_client_transaction* tranx = hyperdex_client_begin_transaction(client_);
 
-        enum hyperdex_client_returncode op_status;
-        struct hyperdex_client_attribute attr;
+    		bangdb_client::connection *conn = tbl_->getconnection();
+    		if(!conn)
+    		{
+    			printf("connection null error, please change the max conn config in bangdb.config, it could be the reason\n");
+    			return -1;
+    		}
+
+    		bangdb_client::bangdb_txn* txn;
+        client_->begin_transaction(txn);
 
         for (auto it = writes.begin(); it != writes.end(); ++it) {
-            code = hyperdex_client_xact_put(tranx, SPACE.c_str(), it->first.c_str(),
-                    it->first.size(), &attr, 1, &op_status);
-            triedTimes = 0;
-            while (loopCode != code) {
-                loopCode = hyperdex_client_loop(client_, 0, &loop_status);
-                triedTimes++;
-                if (triedTimes == 60) {
-                    break;
-                }
-                usleep(50);
-            }
-        }
-        code = hyperdex_client_commit_transaction(tranx, &tranx_status);
-        triedTimes = 0;
-        while (loopCode != code) {
-            loopCode = hyperdex_client_loop(client_, 0, &loop_status);
-            triedTimes++;
-            if (triedTimes == 60) {
-                break;
-            }
-            usleep(50);
+        		bangdb_client::FDT ikey, ival;
+        		ikey.data = it->first.c_str();
+        		ikey.length = it->first.size();
+        		ival.data = it->second.c_str();
+        		ival.length = it->second.size();
+        		if(conn->put(&ikey, &ival, bangdb_client::INSERT_UNIQUE, txn) < 0) {
+        			printf("individual insert transaction failed ");
+        			exit(1);
+
+        		}
         }
 
-        if (tranx_status == HYPERDEX_CLIENT_COMMITTED || tranx_status == HYPERDEX_CLIENT_SUCCESS) {
-            return kOK;
+        if (client_->commit_transaction(txn) >= 0) {
+        		return kOK;
         } else {
-            return kErrorConflict;
+        		return kErrorConflict;
         }
     }
+
 private:
-    struct hyperdex_client* client_;
-    std::vector<std::string> ips_;
+    bangdb_client::database* client_;
+    bangdb_client::table* tbl_;
 };
 } // DB
 } // Ycsb
